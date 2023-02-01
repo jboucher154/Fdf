1c4defb859b392aa1d259bc7a886d642ffff46cf
Jenny Boucher <jebouche@c2r3p4.hive.fi>
2023-02-01 17:58:35 +0200

    free fdf every time, change params for int arr free

--- mlx_close.c
+++ mlx_close.c
@@ -6,7 +6,7 @@
 /*   By: jebouche <jebouche@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/27 12:47:29 by jebouche          #+#    #+#             */
-/*   Updated: 2023/01/31 17:10:55 by jebouche         ###   ########.fr       */
+/*   Updated: 2023/02/01 17:11:43 by jebouche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,12 +26,12 @@ void	free_str_2darr(char **to_free)
 	free(to_free);
 }
 
-void	free_int_2darr(int **to_free)
+void	free_int_2darr(int **to_free, t_fdf_data *fdf)
 {
 	int	i;
 
 	i = 0;
-	while (to_free[i])
+	while (i < fdf->map_size[1])
 	{
 		free(to_free[i]);
 		i++;
@@ -46,21 +46,15 @@ int	mlx_close(t_fdf_data *fdf, int exit_code, char *exit_msg)
 		mlx_destroy_image(fdf->mlx, fdf->img1->img);
 		free(fdf->img1);
 	}
-	if (fdf->img2)
-	{
-		mlx_destroy_image(fdf->mlx, fdf->img2->img);
-		free (fdf->img2);
-	}
 	if (fdf->map)
-		free_int_2darr(fdf->map);
+		free_int_2darr(fdf->map, fdf);
 	if (fdf->camera)
 		free(fdf->camera);
 	if (fdf->win)
 		mlx_destroy_window(fdf->mlx, fdf->win);
 	if (fdf->mlx)
 		free(fdf->mlx);
-	if (fdf)
-		free(fdf);
+	free(fdf);
 	if (exit_code)
 		ft_putendl_fd(exit_msg, 2);
 	exit(exit_code);
