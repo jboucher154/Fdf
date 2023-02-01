ab3c2c6c76735621f95fcfd2198db3855f974a6a
Jenny Boucher <jebouche@c2r3p4.hive.fi>
2023-02-01 18:08:27 +0200

    add new prototypes

--- fdf.h
+++ fdf.h
@@ -6,7 +6,7 @@
 /*   By: jebouche <jebouche@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/31 16:39:49 by jebouche          #+#    #+#             */
-/*   Updated: 2023/01/31 17:11:21 by jebouche         ###   ########.fr       */
+/*   Updated: 2023/02/01 17:06:43 by jebouche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,10 +40,10 @@ typedef struct s_fdf_data
 	void					*mlx;
 	void					*win;
 	struct s_img_data		*img1;
-	struct s_img_data		*img2;
 	struct s_camera			*camera;
 	int						**map;
 	int						map_size[2];
+	int						dirty;
 }							t_fdf_data;
 
 typedef struct s_vector3
@@ -51,6 +51,7 @@ typedef struct s_vector3
 	int	x;
 	int	y;
 	int	z;
+	int	color;
 }				t_vector3;
 
 typedef struct s_camera
@@ -63,7 +64,7 @@ typedef struct s_camera
 }						t_camera;
 
 //for project
-void		draw_line(t_vector3 *f, t_vector3 *s, int color, t_fdf_data *fdf);
+void		draw_line(t_vector3 *f, t_vector3 *s, t_fdf_data *fdf);
 void		my_mlx_pixel_put(t_img_data *data, t_vector3 *start, int color);
 int			mlx_close(t_fdf_data *fdf, int exit_code, char *exit_msg);
 int			handle_press(int key_code, t_fdf_data *fdf);
@@ -73,16 +74,10 @@ t_vector3	*get_camera_view(t_fdf_data *fdf, int i, int j);
 void		new_camera(t_fdf_data *fdf);
 void		draw_camera_view(t_fdf_data *fdf);
 void		free_str_2darr(char **to_free);
+void		set_temps(t_vector3 *one, t_vector3 *two, t_vector3 *first, \
+			t_vector3 *second);
+void		redraw(t_fdf_data *fdf);
+void		get_new_image(t_fdf_data *fdf);
+int			set_line_color(t_vector3 *first, t_vector3 *second);
 
-//for testing
-void		draw_horizontal(t_vector3 *first, t_vector3 *second, int color, \
-			t_fdf_data *fdf);
-void		bresneham_neg_steep(t_vector3 *first, t_vector3 *second, int color, \
-			t_fdf_data *fdf);
-void		bresneham_neg_grad(t_vector3 *first, t_vector3 *second, int color, \
-			t_fdf_data *fdf);
-void		bresneham_pos_steep(t_vector3 *first, t_vector3 *second, int color, \
-			t_fdf_data *fdf);
-void		bresneham_pos_grad(t_vector3 *first, t_vector3 *second, int color, \
-			t_fdf_data *fdf);
 #endif
